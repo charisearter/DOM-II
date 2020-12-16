e56e7e659bf8460eef9de24e36b0ba355fc45f7a
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-12-15 19:48:46 -0600

    headerImg event lister complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+//make variables of what you want events on
+//logo
+const logo = document.querySelector("header > div > h1");
+
+//button and text
+const btn1 = document.querySelector(
+	"section.content-pick > div:nth-child(1) > div"
+);
+const btn1Txt = document.querySelector(
+	"section.content-pick > div:nth-child(1) > p"
+);
+
+// header img
+const headerImg = document.querySelector("header > img");
+
+//add EventListeners
+
+//header image event
+headerImg.addEventListener("mousemove", () => {
+	//do something
+	headerImg.style.filter = "grayscale(100%)";
+});
+
